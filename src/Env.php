f7ab9753dccf23aa7b422af6a1f6c940f0eaf301
lovelock <frostwong@gmail.com>
2016-12-12 17:54:02 +0800

    init commit

---
+++ src/Env.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Frost Wong <frostwong@gmail.com>
+ * Date: 12/12/16
+ * Time: 5:22 PM
+ */
+
+namespace Spe;
+
+
+class Env
+{
+    private $envList = [
+        'dev',
+        'test',
+        'prod',
+        'emu',
+    ];
+
+    /**
+     * Current env setting.
+     *
+     * @var string
+     */
+    private $currentEnv = 'dev';
+
+    /**
+     * Env config path.
+     *
+     * @var string
+     */
+    private $envPath = __DIR__ . '/../../../../env';
+
+    /**
+     * Env constructor.
+     * @param $envList
+     * @param $envPath
+     * @throws EnvException
+     */
+    public function __construct($envList, $envPath)
+    {
+        if (empty($envList)) {
+            $this->envList = $envList;
+        }
+
+        $this->envPath = $envPath;
+
+        $this->currentEnv = file_get_contents($this->envPath);
+        if (!in_array($this->currentEnv, $this->envList, true)) {
+            throw new EnvException('String inside ' . $this->envPath . ' must be element of ' . json_encode($this->envList));
+        }
+    }
+
+    /**
+     * Get the current env setting.
+     *
+     * @return string
+     */
+    public function get()
+    {
+        return $this->currentEnv;
+    }
+
+    /**
+     * Set temporary env config.
+     *
+     * @param $env
+     * @throws EnvException
+     */
+    public function set($env)
+    {
+        if (!in_array($env, $this->envList, true)) {
+            throw new EnvException('Env must be element of ' . json_encode($this->envList) . ', ' . $env . ' is given');
+        }
+
+        $this->currentEnv = $env;
+    }
+}
---
+++ src/EnvException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Frost Wong <frostwong@gmail.com>
+ * Date: 12/12/16
+ * Time: 5:47 PM
+ */
+
+namespace Spe;
+
+
+use Exception;
+
+class EnvException extends Exception
+{
+
+}
